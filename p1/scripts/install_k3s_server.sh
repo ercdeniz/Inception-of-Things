b5b779f1af1d5a264733e6b9e8dd79f31ca205e6
İbrahim Ethem Delemen <152957099+iedelemen@users.noreply.github.com>
2025-07-19 19:17:35 +0300

    fix typo and add control for token

--- p1/scripts/install_k3s_server.sh
+++ p1/scripts/install_k3s_server.sh
@@ -3,10 +3,21 @@
 #K3s belirtilmediği sürece kendini ilk ağ arayüzüne bağlıyor (Örneğin 10.0.2.15-> NAT IP'si). Bunun için aşağıdakileri yaptım.
 #"--bind-adress=192.168.56.110": Sunucunun gelen bağlantıları dinlediği IP adresini belirler.
 #"--node-ip=192.168.56.110": Sunucunun kümedeki diğer üyelere kendini tanıttığı IP adresini belirler. 
-export INSTALL_K3S_EXEC="--bind-adress=192.168.56.110 --node-ip=192.168.56.110"
+export INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --node-ip=192.168.56.110"
 
 curl -sfL https://get.k3s.io | sh -
 
 mkdir -p /vagrant/shared
+
+#Token oluşturulmadan kopyalama işlemi yapmaması için kontrol ekledim.
+SECONDS=0
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
+    if [ "$SECONDS" -ge 60 ]; then
+        echo "Node token not found after 60 seconds of waiting, exiting."
+        exit 1
+    fi
+    echo "Waiting for node-token... ($SECONDS/60s)"
+    sleep 1
+done
 
 cp /var/lib/rancher/k3s/server/node-token /vagrant/shared/node-token
