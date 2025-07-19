f451905635649acca9b6a48d17e388643159ba37
İbrahim Ethem Delemen <152957099+iedelemen@users.noreply.github.com>
2025-07-19 18:30:16 +0300

    fix: Ensure K3s binds to the private network

--- p1/scripts/install_k3s_server.sh
+++ p1/scripts/install_k3s_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#K3s belirtilmediği sürece kendini ilk ağ arayüzüne bağlıyor (Örneğin 10.0.2.15-> NAT IP'si). Bunun için aşağıdakileri yaptım.
+#"--bind-adress=192.168.56.110": Sunucunun gelen bağlantıları dinlediği IP adresini belirler.
+#"--node-ip=192.168.56.110": Sunucunun kümedeki diğer üyelere kendini tanıttığı IP adresini belirler. 
+export INSTALL_K3S_EXEC="--bind-adress=192.168.56.110 --node-ip=192.168.56.110"
+
 curl -sfL https://get.k3s.io | sh -
 
 mkdir -p /vagrant/shared
