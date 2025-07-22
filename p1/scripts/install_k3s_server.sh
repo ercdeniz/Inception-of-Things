bb471fd801830f1807b0d2fc7e0545dbdc94e2c1
TufanKurukaya <kurukayatufan@gmail.com>
2025-07-22 18:03:08 +0300

    Refactor Vagrantfile and install_k3s_server.sh; remove unused YAML files and add my-apps.yaml for application deployments

--- p1/scripts/install_k3s_server.sh
+++ p1/scripts/install_k3s_server.sh
@@ -3,7 +3,7 @@
 #K3s belirtilmediği sürece kendini ilk ağ arayüzüne bağlıyor (Örneğin 10.0.2.15-> NAT IP'si). Bunun için aşağıdakileri yaptım.
 #"--bind-adress=192.168.56.110": Sunucunun gelen bağlantıları dinlediği IP adresini belirler.
 #"--node-ip=192.168.56.110": Sunucunun kümedeki diğer üyelere kendini tanıttığı IP adresini belirler. 
-export INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --node-ip=192.168.56.110"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.56.110 --node-ip=192.168.56.110"
 
 curl -sfL https://get.k3s.io | sh -
 
