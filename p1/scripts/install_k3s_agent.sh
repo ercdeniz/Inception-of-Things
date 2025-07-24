4eb4972cadf2b66962119214cded2e7bebb5f899
TufanKurukaya <kurukayatufan@gmail.com>
2025-07-24 15:31:13 +0300

    fixed basic bugs

--- p1/scripts/install_k3s_agent.sh
+++ p1/scripts/install_k3s_agent.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update
+apt-get install -y curl
 
 for i in {1..20}; do
   if [ -f /vagrant/shared/node-token ]; then
--- p1/scripts/install_k3s_server.sh
+++ p1/scripts/install_k3s_server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#K3s belirtilmediği sürece kendini ilk ağ arayüzüne bağlıyor (Örneğin 10.0.2.15-> NAT IP'si). Bunun için aşağıdakileri yaptım.
-#"--bind-adress=192.168.56.110": Sunucunun gelen bağlantıları dinlediği IP adresini belirler.
-#"--node-ip=192.168.56.110": Sunucunun kümedeki diğer üyelere kendini tanıttığı IP adresini belirler. 
+
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.56.110 --node-ip=192.168.56.110"
+
+apt-get update
+apt-get install -y curl
 
 curl -sfL https://get.k3s.io | sh -
 
 mkdir -p /vagrant/shared
 
-#Token oluşturulmadan kopyalama işlemi yapmaması için kontrol ekledim.
 SECONDS=0
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
     if [ "$SECONDS" -ge 60 ]; then
